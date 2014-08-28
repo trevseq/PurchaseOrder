e73c29272fe3209bb70645157e975445c3e4d912
trevseq <trvseq9910@gmail.com>
2014-08-28 12:29:35 -0400

    removed modernizr, cleaned up bundles, more cdns

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -10,19 +10,10 @@ namespace PurchaseOrder
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            // CDN support!
-            bundles.UseCdn = true;
-            var jqueryJsCdnPath = "//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js";
-            
-            bundles.Add(new ScriptBundle("~/bundles/jquery", jqueryJsCdnPath).Include(
-                "~/Scripts/jquery-{version}.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
-                "~/Scripts/custom.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-            //    "~/Scripts/knockout-{version}.js",
-            //    "~/Scripts/knockout.validation.js"));
+                "~/Scripts/custom.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
                 "~/Scripts/sammy-{version}.js",
@@ -32,29 +23,13 @@ namespace PurchaseOrder
                 "~/Scripts/app/home.viewmodel.js",
                 "~/Scripts/app/_run.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/jquery-ui.css",
                 "~/Content/bootstrap.css",
                 "~/Content/formpage.css"));
 
             bundles.Add(new StyleBundle("~/bundles/editstyles").Include(
                 "~/Content/bootstrap.css",
-                "~/Content/jquery-ui.css",
                 "~/Content/editpage.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                "~/Scripts/jquery-ui.js"
-                ));
-
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
