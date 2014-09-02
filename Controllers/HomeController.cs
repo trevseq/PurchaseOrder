6447ef375bbc8116b518b29a15ac924ad54fabd5
trevseq <trvseq9910@gmail.com>
2014-09-02 17:25:01 -0400

    saving and deleting entries finished

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -345,11 +345,14 @@ namespace PurchaseOrder.Controllers
             {
                 var db = new PurchaseOrdersEntities();
                 var t = db.Vendors;
+                var t2 = db.Vendors_Contact;
                 var vendor = t.Where(e => e.Id == id).FirstOrDefault();
+                var contact = t2.Where(e => e.VendorId == id).FirstOrDefault();
 
-                // delete the vendor row
-
-                // if vendor has a contact, delete it too
+                // delete the vendor row and contact row
+                t.Remove(vendor);
+                t2.Remove(contact);
+                
                 success = true;
             }
             else if (table == "products")
@@ -359,6 +362,7 @@ namespace PurchaseOrder.Controllers
                 var product = t.Where(e => e.Id == id).FirstOrDefault();
 
                 // delete the product row
+                t.Remove(product);
                 success = true;
             }
             else if (table == "terms")
@@ -368,6 +372,7 @@ namespace PurchaseOrder.Controllers
                 var term = t.Where(e => e.Id == id).FirstOrDefault();
 
                 // delete the payment term row
+                t.Remove(term);
                 success = true;
             }
 
@@ -378,49 +383,132 @@ namespace PurchaseOrder.Controllers
             };
         }
 
-        public ActionResult SaveTerm(int id, string name, string value)
+        public ActionResult SaveTerm(int? id, string name, string value)
         {
             bool success = false;
-            var db = new PurchaseOrdersEntities();
+            try
+            {
+                var db = new PurchaseOrdersEntities();
 
-
+                if (id == null)
+                {
+                    var newTerm = new PaymentTerm() { Name = name, Value = value };
+                    db.PaymentTerms.Add(newTerm);
+                    db.SaveChanges();
+                }
+                else if (id != null)
+                {
+                    var t = db.PaymentTerms;
+                    var term = t.Where(e => e.Id == id).FirstOrDefault();
+                    term.Name = name;
+                    term.Value = value;
+                    db.SaveChanges();
+                }
+                success = true;
+            }
+            catch { }
             return new JsonResult()
             {
                 Data = success,
                 JsonRequestBehavior = JsonRequestBehavior.AllowGet
             };
         }
-        public ActionResult SaveProds(int id, string name)
+        public ActionResult SaveProds(int? id, string name)
         {
             bool success = false;
-            var db = new PurchaseOrdersEntities();
+            try
+            {
+                var db = new PurchaseOrdersEntities();
 
-
-
+                if (id == null)
+                {
+                    var newProd = new Product() { Name = name };
+                    db.Products.Add(newProd);
+                    db.SaveChanges();
+                }
+                else if (id != null)
+                {
+                    var t = db.Products;
+                    var prod = t.Where(e => e.Id == id).FirstOrDefault();
+                    prod.Name = name;
+                    db.SaveChanges();
+                }
+                success = true;
+            }
+            catch { }
             return new JsonResult()
             {
                 Data = success,
                 JsonRequestBehavior = JsonRequestBehavior.AllowGet
             };
         }
-        public ActionResult SaveVend(int id, 
+        public ActionResult SaveVend(int? id, 
             string name, 
-            string? website, 
-            string? address, 
-            string? comments,
-            string? vendName,
-            string? vendTitle,
-            string? vendEmail,
-            string? vendPhone,
-            string? vendExt,
-            string? vendFax)
+            string website, 
+            string address, 
+            string comments,
+            string vendName,
+            string vendTitle,
+            string vendEmail,
+            string vendPhone,
+            string vendExt,
+            string vendFax)
         {
             bool success = false;
-            var db = new PurchaseOrdersEntities();
+            try
+            {
+                var db = new PurchaseOrdersEntities();
+                if (id == null)
+                {
+                    var newVend = new Vendor()
+                    {
+                        Name = name,
+                        Website = website,
+                        Address1 = address,
+                        Comments = comments
+                    };
+                    db.Vendors.Add(newVend);
+                    db.SaveChanges();
+                    if (vendName != null || vendTitle != null || vendEmail != null || vendPhone != null || vendExt != null || vendFax != null)
+                    {
+                        var newVContact = new Vendors_Contact()
+                                            {
+                                                Name = vendName,
+                                                Title = vendTitle,
+                                                Email = vendEmail,
+                                                Phone = vendPhone,
+                                                Ext = vendExt,
+                                                Fax = vendFax
+                                            };
+                        db.Vendors_Contact.Add(newVContact);
+                        db.SaveChanges();
+                    }
+                    
+                }
+                else if (id != null)
+                {
+                    var t = db.Vendors;
+                    var t2 = db.Vendors_Contact;
+                    var vend = t.Where(e => e.Id == id).FirstOrDefault();
+                    var cont = t2.Where(e => e.VendorId == id).FirstOrDefault();
 
+                    vend.Name = name;
+                    vend.Website = website;
+                    vend.Address1 = address;
+                    vend.Comments = comments;
 
+                    cont.Name = vendName;
+                    cont.Title = vendTitle;
+                    cont.Email = vendEmail;
+                    cont.Phone = vendPhone;
+                    cont.Ext = vendExt;
+                    cont.Fax = vendFax;
 
-
+                    db.SaveChanges();
+                }
+                success = true;
+            }
+            catch { }
             return new JsonResult()
             {
                 Data = success,
